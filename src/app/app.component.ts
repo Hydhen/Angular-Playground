6e6e38d160736264dd173a564eae8c6e26ea90c2
Hydhen <juillet.loic@gmail.com>
2021-10-26 19:05:48 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, VERSION } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  private userBS = new BehaviorSubject<any>(undefined);
+  public user$ = this.userBS.asObservable();
+
+  public selected(id: number): void {
+    this.userBS.next({ id: id });
+  }
 }
